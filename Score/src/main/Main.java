2ea7db36b6381c4c76e797abfc91570f1e739735
anantajit <winnernoend@gmail.com>
2017-03-18 14:27:42 -0700

    Improved physics in demo

--- Score/src/main/Main.java
+++ Score/src/main/Main.java
@@ -13,19 +13,36 @@ public class Main {
 		sprite1.height = 200;
 		sleep(100);
 		while(true){
-			if(Listener.keypressed.equals("w")){
-				sprite1.y--;
+			if(Listener.keypressed.equals("w")&&sprite1.y>=700){
+				for(double t = 0; sprite1.y<=700; t++){
+					sprite1.y = sprite1.y - (int) (4*t - 3*t*t/20);
+					if(sprite1.y > 700){
+						sprite1.y = 700;
+						break;
+					}
+					sleep(15);
+					Canvas.panel.repaint();
+				}
+			}
+			else if(sprite1.y<700){
+				for(double t = 0; sprite1.y<=700; t++){
+					sprite1.y = sprite1.y - (int) (-3*t*t/20);
+					if(sprite1.y > 700){
+						sprite1.y = 700;
+						break;
+					}
+					sleep(15);
+					Canvas.panel.repaint();
+				}
 			}
 			if(Listener.keypressed.equals("a")){
 				sprite1.x--;
-			}
-			if(Listener.keypressed.equals("s")){
-				sprite1.y++;
 			}
 			if(Listener.keypressed.equals("d")){
 				sprite1.x++;
 			}
 			sleep(1);
+			//simplest physics
 			Canvas.panel.repaint();
 		}
 	}
