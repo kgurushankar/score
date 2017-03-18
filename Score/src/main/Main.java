d820acf6f5bb363f3473c758d5faef80d9aab2cd
anantajit <winnernoend@gmail.com>
2017-03-18 10:14:13 -0700

    Slight changes to the test case

--- Score/src/main/Main.java
+++ Score/src/main/Main.java
@@ -4,22 +4,29 @@ import core.Canvas;
 import sprite.Sprite;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args){
 		Canvas.startCanvas();
 		Sprite sprite1 = new Sprite();
 		sprite1.width = 200;
 		sprite1.height = 200;
-		Thread.sleep(100);
+		sleep(100);
 		while(true){
 			sprite1.goTo(0,0);
-			Thread.sleep(100);
+			sleep(100);
 			sprite1.goTo(100, 0);
-			Thread.sleep(100);
+			sleep(100);
 			sprite1.goTo(100, 100);
-			Thread.sleep(100);
+			sleep(100);
 			sprite1.goTo(0, 100);
-			Thread.sleep(100);
+			sleep(100);
 		}
-		
+	}
+	public static void sleep(int millis){
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
