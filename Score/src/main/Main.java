b808efa952fe25eb0d4443698ba20a8b7c865a2a
anantajit <winnernoend@gmail.com>
2017-03-17 17:38:37 -0700

    NANANANANANANANANANANANANANANANA Batman!

--- Score/src/main/Main.java
+++ Score/src/main/Main.java
@@ -9,8 +9,18 @@ public class Main {
 	
 	public static void main(String[] args) throws InterruptedException {
 		Canvas.startCanvas();
-		Sprite sprite1 = new Sprite(100,100,100,100);
-
+		Sprite sprite1 = new Sprite();
+		Thread.sleep(100);
+		while(true){
+			sprite1.goTo(0,0);
+			Thread.sleep(100);
+			sprite1.goTo(100, 0);
+			Thread.sleep(100);
+			sprite1.goTo(100, 100);
+			Thread.sleep(100);
+			sprite1.goTo(0, 100);
+			Thread.sleep(100);
+		}
 		
 	}
 }
